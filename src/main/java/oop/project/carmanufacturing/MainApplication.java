3970803dd61dab68e29acec41efe00274b256efd
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-03-25 09:12:38 +0600

    initial commit

--- src/main/java/oop/project/carmanufacturing/MainApplication.java
+++ src/main/java/oop/project/carmanufacturing/MainApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Car Manufacturing");
         stage.setScene(scene);
         stage.show();
     }
