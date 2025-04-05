1e830196af689bc01fa2e03399d3f3f2cbed3f2f
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-04-06 05:41:40 +0600

    added on action for 4 goals primarily

--- src/main/java/oop/project/carmanufacturing/Supto_1930875/ProductionManager.java
+++ src/main/java/oop/project/carmanufacturing/Supto_1930875/ProductionManager.java
@@ -2,4 +2,15 @@ package oop.project.carmanufacturing.Supto_1930875;
 
 public class ProductionManager
 {
+    public void showScheduleTasks(ActionEvent actionEvent) {
+    }
+
+    public void showMonitorProgress(ActionEvent actionEvent) {
+    }
+
+    public void showAssignShifts(ActionEvent actionEvent) {
+    }
+
+    public void showGenerateReport(ActionEvent actionEvent) {
+    }
 }
