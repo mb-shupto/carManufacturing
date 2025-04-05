9bf8eed107ddf2c4eef760eb8888103d72dab662
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-04-06 02:27:17 +0600

    irrelevant for now

--- src/main/java/oop/project/carmanufacturing/LoginController.java
+++ src/main/java/oop/project/carmanufacturing/LoginController.java
@@ -1,6 +1,6 @@
 package oop.project.carmanufacturing;
 
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
