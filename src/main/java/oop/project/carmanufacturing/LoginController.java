4ed277e5c52ce037d720760339b45f3810098227
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-04-05 21:53:38 +0600

    Modified the login.fxml and LoginController class accordingly

--- src/main/java/oop/project/carmanufacturing/LoginController.java
+++ src/main/java/oop/project/carmanufacturing/LoginController.java
@@ -1,26 +1,21 @@
 package oop.project.carmanufacturing;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.util.List;
 
 public class LoginController {
+
+    private List<User> userList;
     @javafx.fxml.FXML
-    private TextField usernameField;
+    private ComboBox<User> userComboBox;
     @javafx.fxml.FXML
     private PasswordField passwordField;
     @javafx.fxml.FXML
     private Label messageLabel;
-
-    private List<User> userList;
 
     @javafx.fxml.FXML
     public void initialize() {
@@ -31,15 +26,9 @@ public class LoginController {
     @javafx.fxml.FXML
     public void login(ActionEvent actionEvent) throws IOException {
         for (User u: userList) {
-            if (usernameField.getText().equals(u.getUsername())
+            if (userComboBox.getItems().equals(u.getUsername())
                     && passwordField.getText().equals(u.getPassword())) {
                 messageLabel.setText("Log in successful!");
-                // Now switch to the other scene
-                Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
-                Stage stage = (Stage) messageLabel.getScene().getWindow();
-                stage.setScene(new Scene(root));
-
-                return;
             }
         }
         messageLabel.setText("Invalid username or password!");
