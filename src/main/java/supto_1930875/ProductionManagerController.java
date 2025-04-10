ae49a06477102c5914e5868db1b2bca404747d9e
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-04-11 01:42:20 +0600

    added controller class for production manager

---
+++ src/main/java/supto_1930875/ProductionManagerController.java
@@ -0,0 +1,31 @@
+package supto_1930875;
+
+import javafx.event.ActionEvent;
+
+public class ProductionManagerController {
+
+
+    @javafx.fxml.FXML
+    public void scheduleTaskOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void monitorTaskOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignShiftsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateReportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void orderMaterialsOnAction(ActionEvent actionEvent) {
+    }
+}
