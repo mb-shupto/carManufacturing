9746a9e48f041498f46147f289452b7828706782
Hasibul <syedhasib691@gmail.com>
2025-04-11 11:53:53 +0600

    create user 6 - goal 1,2

---
+++ src/main/java/Hasibul_2321368/Schedule-Vehicle-Maintenance-View1.java
@@ -0,0 +1,58 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class Schedule-Vehicle-Maintenance-View1
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
+
+@javafx.fxml.FXML
+private TableColumn TaskStatusTableColumn;
+@javafx.fxml.FXML
+private TextField IssueDescriptionTextField;
+@javafx.fxml.FXML
+private TextField VehicleIdTextField;
+@javafx.fxml.FXML
+private TableColumn ScheduledDateTableColumn;
+@javafx.fxml.FXML
+private TableColumn RequestDateTableColumn;
+@javafx.fxml.FXML
+private Button CancelButton;
+@javafx.fxml.FXML
+private TableColumn DeleveryDateTableColumn;
+@javafx.fxml.FXML
+private DatePicker ScheduledDateDatePicker;
+@javafx.fxml.FXML
+private TextField VehicleInformationTextField;
+@javafx.fxml.FXML
+private TableColumn VehicleIDTableColumn;
+@javafx.fxml.FXML
+private TableColumn TechnicianAssignTableColumn;
+@javafx.fxml.FXML
+private Button ConfirmButton;
+@javafx.fxml.FXML
+private ComboBox TaskStatusComboBox;
+@javafx.fxml.FXML
+private TextField RequestIDTextField;
+@javafx.fxml.FXML
+private TableColumn RequestIDTableColumn;
+@javafx.fxml.FXML
+private TableColumn IssueDescriptionTableColumn;
+@javafx.fxml.FXML
+private CheckBox SparePartsVerificationCheckBox;
+@javafx.fxml.FXML
+private DatePicker RequestDateDatePicker;
+@javafx.fxml.FXML
+private ComboBox TechnicianAssignedComboBox;
+
+@javafx.fxml.FXML
+public void ConfirmButtonOnAction(ActionEvent actionEvent) {
+}
+
+@javafx.fxml.FXML
+public void CancelButtonOnAction(ActionEvent actionEvent) {
+}
+
+public void main() {
+}
