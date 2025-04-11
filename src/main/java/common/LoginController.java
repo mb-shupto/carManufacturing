c5b54c33097ebd4aaa5ceaf68f739ee6f3d4ac3e
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-04-11 20:05:22 +0600

    changed temporary function for the login button

--- src/main/java/common/LoginController.java
+++ src/main/java/common/LoginController.java
@@ -1,24 +1,41 @@
 package common;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class LoginController {
     @FXML
     public ComboBox<String> userComboBox;
     @FXML
     private PasswordField passwordField;
-    @FXML
-    private Label messageLabel;
 
     private UserManager userManager = new UserManager();
+    @FXML
+    private Label outputLabel;
+
+    private List<String[]> credentials = new ArrayList<>();
 
     @FXML
     public void initialize() {
-        // putting items on the combo box
-        userComboBox.getItems().addAll(userManager.getAllUsernames());
+        credentials.add(new String[]{"Production Manager","1234"});
+        credentials.add(new String[]{"QA Engineer", "1122"});
+
+        for (String[] credential : credentials){
+            userComboBox.getItems().add(credential[0]);
+        }
     }
 
     @FXML
@@ -27,9 +44,45 @@ public class LoginController {
         String password = passwordField.getText();
 
         if (userManager.validateLogin(username, password)) {
-            messageLabel.setText("Login successful!");
+            outputLabel.setText("Login successful!");
         } else {
-            messageLabel.setText("Invalid credentials!");
+            outputLabel.setText("Invalid credentials!");
         }
+    }
+
+    @FXML
+    public void loginOnAction(ActionEvent actionEvent) throws IOException {
+        String user = userComboBox.getValue();
+        String password = passwordField.getText();
+
+            if (user != null && password != null){
+                for (String[] credential : credentials){
+                    if (credential[0].equals(user) && credential[1].equals(password));
+                    try {
+                        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/supto_1930875/productionManager.fxml"));
+                        Parent root = fxmlLoader.load();
+                        Node source = (Node) actionEvent.getSource();
+                        Stage stage = (Stage) source.getScene().getWindow();
+                        stage.setScene(new Scene(root));
+                        stage.setTitle("Production Manager Dashboard");
+                        stage.show();
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                        outputLabel.setText("Error loading the dashboard!");
+                        return;
+                    }
+                }
+            }
+            outputLabel.setText("Invalid User or Password");
+
+    }
+
+    public List<String[]> getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(List<String[]> credentials) {
+        this.credentials = credentials;
     }
 }
