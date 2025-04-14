f57bc6700e71178a08048d06c3eba233cae7a958
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-04-14 23:05:27 +0600

    updated fxml

--- src/main/java/anamika_2220139/inventoryManagerController.java
+++ src/main/java/anamika_2220139/inventoryManagerController.java
@@ -1,4 +1,11 @@
 package anamika_2220139;
 
 public class inventoryManagerController {
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateInventoryOnAction(ActionEvent actionEvent) {
+    }
 }
