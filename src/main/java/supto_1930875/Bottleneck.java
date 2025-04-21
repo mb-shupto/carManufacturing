86ae76252ba907e6d8e41d447b668e0e6b6f7dcf
MD. MASUM-UL-BARI SUPTO <mr.shupto09@gmail.com>
2025-04-21 22:52:22 +0600

    few changes

--- src/main/java/supto_1930875/Bottleneck.java
+++ src/main/java/supto_1930875/Bottleneck.java
@@ -1,7 +1,37 @@
 package supto_1930875;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.beans.property.SimpleStringProperty;
 
 public class Bottleneck {
+    private final SimpleStringProperty bottleneckName;
+    private final SimpleStringProperty status;
+
+    public Bottleneck(String bottleneckName, String status) {
+        this.bottleneckName = new SimpleStringProperty(bottleneckName);
+        this.status = new SimpleStringProperty(status);
+    }
+
+    public String getBottleneckName() {
+        return bottleneckName.get();
+    }
+
+    public SimpleStringProperty bottleneckNameProperty() {
+        return bottleneckName;
+    }
+
+    public void setBottleneckName(String bottleneckName) {
+        this.bottleneckName.set(bottleneckName);
+    }
+
+    public String getStatus() {
+        return status.get();
+    }
+
+    public SimpleStringProperty statusProperty() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status.set(status);
+    }
 }
--- src/main/java/supto_1930875/Task.java
+++ src/main/java/supto_1930875/Task.java
@@ -1,4 +1,37 @@
 package supto_1930875;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Task {
+    private final SimpleStringProperty taskName;
+    private final SimpleStringProperty progress;
+
+    public Task(String taskName, String progress) {
+        this.taskName = new SimpleStringProperty(taskName);
+        this.progress = new SimpleStringProperty(progress);
+    }
+
+    public String getTaskName() {
+        return taskName.get();
+    }
+
+    public SimpleStringProperty taskNameProperty() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName.set(taskName);
+    }
+
+    public String getProgress() {
+        return progress.get();
+    }
+
+    public SimpleStringProperty progressProperty() {
+        return progress;
+    }
+
+    public void setProgress(String progress) {
+        this.progress.set(progress);
+    }
 }
