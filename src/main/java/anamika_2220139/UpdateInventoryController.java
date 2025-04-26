d511ef35f8b9e0483c25a797a92e094eb2e185c0
Anamika Saha Anna <anamikasahaanna@gmail.com>
2025-04-26 22:11:38 +0600

    new class controller with fxml

---
+++ src/main/java/anamika_2220139/UpdateInventoryController.java
@@ -0,0 +1,44 @@
+package anamika_2220139;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class UpdateInventoryController
+{
+    @javafx.fxml.FXML
+    private TableColumn materialNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn quantityColumn;
+    @javafx.fxml.FXML
+    private Button verifyButton;
+    @javafx.fxml.FXML
+    private Button backButton;
+    @javafx.fxml.FXML
+    private Label selectedMaterialLabel;
+    @javafx.fxml.FXML
+    private Button updateButton;
+    @javafx.fxml.FXML
+    private TextField quantityField;
+    @javafx.fxml.FXML
+    private TableView materialsTableView;
+    @javafx.fxml.FXML
+    private TextField qualityField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @javafx.fxml.FXML
+    public void updateInventoryLevelsOnAction(ActionEvent actionEvent) {
+
+    }
+}
