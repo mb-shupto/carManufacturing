82043d7c7614f4018d8a6ac82a65d69ffaf3ff70
Hasibul <syedhasib691@gmail.com>
2025-04-27 21:45:07 +0600

    login part done

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,9 @@ module oop.project.carmanufacturing {
 
     opens common to javafx.fxml;
     opens supto_1930875 to javafx.fxml;
+    opens Hasibul_2321368 to javafx.fxml;
 
     exports supto_1930875;
+    exports Hasibul_2321368;
     exports common;
 }
